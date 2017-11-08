366a1355f1c8818bd1008bc9447d5de9988e5698
林松斌 <linsongbin@126.com>
2017-11-08 11:04:59 +0800

    fix

---
+++ src/LinFx.Data/MongoDB/MongoDbRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace LinFx.Data.MongoDB
+{
+    public class MongoDbRepository<TEntity>
+    {
+        private readonly IMongoDatabaseProvider _databaseProvider;
+
+        public MongoDbRepository(IMongoDatabaseProvider databaseProvider)
+        {
+            _databaseProvider = databaseProvider;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        //public virtual MongoCollection<TEntity> Collection
+        //{
+        //    get
+        //    {
+        //        return _databaseProvider.Database.GetCollection<TEntity>(typeof(TEntity).Name);
+        //    }
+        //}
+    }
+}
