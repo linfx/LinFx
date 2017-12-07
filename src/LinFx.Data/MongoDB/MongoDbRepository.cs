2aba57e8da56db26e3e09c41aa9634e6e104a2a3
林松斌 <linsongbin@126.com>
2017-12-07 23:00:26 +0800

    fix mongodb

--- src/LinFx.Data/MongoDB/MongoDbRepository.cs
+++ src/LinFx.Data/MongoDB/MongoDbRepository.cs
@@ -1,39 +1,36 @@
-﻿//using LinFx.Domain.Entities;
-//using MongoDB.Driver;
-//using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using MongoDB.Driver;
+using LinFx.Domain.Entities;
 
-//namespace LinFx.Data.MongoDB
-//{
-//    public class MongoDbRepository<TEntity>
-//        where TEntity : class, IEntity
-//    {
-//        private readonly IMongoDatabaseProvider _provider;
+namespace LinFx.Data.MongoDB
+{
+    public class MongoDbRepository<TEntity> where TEntity : class, IEntity
+    {
+        private readonly IMongoDatabaseProvider _provider;
 
-//        public MongoDbRepository(IMongoDatabaseProvider databaseProvider)
-//        {
-//            _provider = databaseProvider;
-//        }
+        public MongoDbRepository(IMongoDatabaseProvider databaseProvider)
+        {
+            _provider = databaseProvider;
+        }
 
-//        public virtual IMongoCollection<TEntity> Collection
-//        {
-//            get { return _provider.Database.GetCollection<TEntity>(typeof(TEntity).Name); }
-//        }
+        private IMongoCollection<TEntity> _collection
+        {
+            get { return _provider.Database.GetCollection<TEntity>(typeof(TEntity).Name); }
+        }
 
-//        public Task InsertAsync(TEntity item)
-//        {
-//            return Collection.InsertOneAsync(item);
-//        }
+        public Task InsertAsync(TEntity item)
+        {
+            return _collection.InsertOneAsync(item);
+        }
 
-//        public Task DeleteAsync(TEntity item)
-//        {
-//            return Collection.DeleteOneAsync(item.Id);
-//        }
+        //public Task DeleteAsync(TEntity item)
+        //{
+        //    return _collection.DeleteOneAsync<TEntity>(item);
+        //}
 
-
-
-//        //public Task UpdateAsync(TEntity item)
-//        //{
-//        //    return Collection.UpdateOneAsync(item.Id, item);
-//        //}
-//    }
-//}
+        //public Task UpdateAsync(TEntity item)
+        //{
+        //    return Collection.UpdateOneAsync(item.Id, item);
+        //}
+    }
+}
