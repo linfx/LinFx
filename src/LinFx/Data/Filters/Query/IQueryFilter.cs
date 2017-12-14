e2a59da7847bdbab9c156590e59121cf9daf0a52
林松斌 <linsongbin@126.com>
2017-12-14 22:40:26 +0800

    合并 LinFx.Data

---
+++ src/LinFx/Data/Filters/Query/IQueryFilter.cs
@@ -0,0 +1,14 @@
+﻿using LinFx.Data.Extensions;
+using System;
+using System.Linq.Expressions;
+
+namespace LinFx.Data.Filters.Query
+{
+	public interface IQueryFilter
+    {
+        string FilterName { get; }
+        bool IsEnabled { get; }
+        IFieldPredicate ExecuteFilter<TEntity>() where TEntity : class;
+        Expression<Func<TEntity, bool>> ExecuteFilter<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class;
+    }
+}
---
+++ src/LinFx/Data/Filters/Query/SoftDeleteQueryFilter.cs
@@ -0,0 +1,51 @@
+﻿using LinFx.Data.Extensions;
+using LinFx.Data.Utils;
+using LinFx.Domain.Entities;
+using LinFx.Domain.Uow;
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace LinFx.Data.Filters.Query
+{
+	public class SoftDeleteQueryFilter : IQueryFilter
+    {
+        public string FilterName => DataFilters.SoftDelete;
+
+        public bool IsEnabled => false;
+
+        public bool IsDeleted => false;
+
+        private bool IsFilterable<TEntity>() where TEntity : class
+        {
+            return typeof(ISoftDelete).IsAssignableFrom(typeof(TEntity)) && IsEnabled;
+        }
+
+        public IFieldPredicate ExecuteFilter<TEntity>() where TEntity : class
+        {
+            IFieldPredicate predicate = null;
+            if (IsFilterable<TEntity>())
+                predicate = Predicates.Field<TEntity>(f => (f as ISoftDelete).IsDeleted, Operator.Eq, IsDeleted);
+
+            return predicate;
+        }
+
+        public Expression<Func<TEntity, bool>> ExecuteFilter<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class
+        {
+            if (IsFilterable<TEntity>())
+            {
+                var propType = typeof(TEntity).GetProperty(nameof(ISoftDelete.IsDeleted));
+                if (predicate == null)
+                    predicate = ExpressionUtils.MakePredicate<TEntity>(nameof(ISoftDelete.IsDeleted), IsDeleted, propType.PropertyType);
+                else
+                {
+                    ParameterExpression paramExpr = predicate.Parameters[0];
+                    MemberExpression memberExpr = Expression.Property(paramExpr, nameof(ISoftDelete.IsDeleted));
+                    BinaryExpression body = Expression.AndAlso(predicate.Body, Expression.Equal(memberExpr, Expression.Constant(IsDeleted, propType.PropertyType)));
+                    predicate = Expression.Lambda<Func<TEntity, bool>>(body, paramExpr);
+                }
+            }
+            return predicate;
+        }
+    }
+}
