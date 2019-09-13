184f348a6b96a364a2bcc81722e9fdb4c45dd9e9
林松斌 <linsongbin@126.com>
2019-09-13 22:18:58 +0800

    update ObjectMappingTest

--- test/LinFx.Test/ObjectMapping/ObjectMappingTest.cs
+++ test/LinFx.Test/ObjectMapping/ObjectMappingTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace LinFx.Test.ObjectMapping
@@ -6,7 +7,7 @@ namespace LinFx.Test.ObjectMapping
     public class ObjectMappingTest
     {
         [Fact]
-        public void MapToTests()
+        public void MapTo_Tests()
         {
             var person = new Person()
             {
@@ -14,10 +15,28 @@ namespace LinFx.Test.ObjectMapping
                 Age = 200,
             };
 
-            var dto = person.MapTo<PersonDto>(person);
+            var dto = person.MapTo<PersonDto>();
 
             Assert.Equal(person.Name, dto.Name);
             Assert.Equal(person.Age, dto.Age);
+        }
+
+        [Fact]
+        public void MapTo_Array_Tests()
+        {
+            var persions = new List<Person>
+            {
+                new Person { Name = "Lio1", Age = 11 },
+                new Person { Name = "Lio1", Age = 12 },
+                new Person { Name = "Lio1", Age = 13 },
+            }.ToArray();
+
+            var dtos = persions.MapTo<PersonDto[]>();
+            Assert.Equal(persions.Length, dtos.Length);
+
+            var dtos2 = new PersonDto[persions.Length];
+            persions.MapTo(dtos2);
+            Assert.Equal(persions.Length, dtos2.Length);
         }
     }
 }
