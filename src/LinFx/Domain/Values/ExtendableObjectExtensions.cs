f0715cadb52aaeb002f49606dd3a7e4e7f9e0d13
林松斌 <linsongbin@126.com>
2019-12-04 21:11:52 +0800

    优化目录

--- src/LinFx/Domain/Values/ExtendableObjectExtensions.cs
+++ src/LinFx/Domain/Values/ExtendableObjectExtensions.cs
@@ -1,6 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using LinFx.Utils;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using ShopFx.Helpers;
 using System;
 using System.Collections.Generic;
 
--- src/LinFx/Utils/TypeExtensions.cs
+++ src/LinFx/Utils/TypeExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace LinFx
 {
@@ -32,6 +33,56 @@ namespace LinFx
         public static bool IsAssignableTo(this Type type, Type targetType)
         {
             return targetType.IsAssignableFrom(type);
+        }
+    }
+
+    /// <summary>
+    /// Some simple type-checking methods used internally.
+    /// </summary>
+    internal static class TypeHelper
+    {
+        public static bool IsFunc(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            var type = obj.GetType();
+            if (!type.GetTypeInfo().IsGenericType)
+                return false;
+
+            return type.GetGenericTypeDefinition() == typeof(Func<>);
+        }
+
+        public static bool IsFunc<TReturn>(object obj)
+        {
+            return obj != null && obj.GetType() == typeof(Func<TReturn>);
+        }
+
+        public static bool IsPrimitiveExtendedIncludingNullable(Type type, bool includeEnums = false)
+        {
+            if (IsPrimitiveExtended(type, includeEnums))
+                return true;
+
+            if (type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+                return IsPrimitiveExtended(type.GenericTypeArguments[0], includeEnums);
+
+            return false;
+        }
+
+        private static bool IsPrimitiveExtended(Type type, bool includeEnums)
+        {
+            if (type.GetTypeInfo().IsPrimitive)
+                return true;
+
+            if (includeEnums && type.GetTypeInfo().IsEnum)
+                return true;
+
+            return type == typeof(string) ||
+                   type == typeof(decimal) ||
+                   type == typeof(DateTime) ||
+                   type == typeof(DateTimeOffset) ||
+                   type == typeof(TimeSpan) ||
+                   type == typeof(Guid);
         }
     }
 }
