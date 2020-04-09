0549124c4dc5dd9265ae302ff507e42f2f727bdf
Mr.Lin <linsongbin@126.com>
2020-04-09 08:43:49 +0800

    NavigationServiceCollectionExtensions

---
+++ src/LinFx/Extensions/UI.Navigation/NavigationServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using LinFx.Extensions.UI.Navigation;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class NavigationServiceCollectionExtensions
+    {
+        public static LinFxBuilder AddNavigation(this LinFxBuilder builder, Action<NavigationOptions> optionsAction)
+        {
+            builder.Services.Configure(optionsAction);
+            builder.Services.AddSingleton<IMenuManager, MenuManager>();
+            return builder;
+        }
+    }
+}
