fe7b2c6fcc9aa6946768985d6e37d1f398bde469
Mr.Lin <linsongbin@126.com>
2021-12-15 22:48:27 +0800

    中文注释

--- src/LinFx/Extensions/Uow/IUnitOfWorkOptions.cs
+++ src/LinFx/Extensions/Uow/IUnitOfWorkOptions.cs
@@ -2,8 +2,14 @@
 
 namespace LinFx.Extensions.Uow
 {
+    /// <summary>
+    /// 工作单元配置
+    /// </summary>
     public interface IUnitOfWorkOptions
     {
+        /// <summary>
+        /// 是否事务
+        /// </summary>
         bool IsTransactional { get; }
 
         IsolationLevel? IsolationLevel { get; }
