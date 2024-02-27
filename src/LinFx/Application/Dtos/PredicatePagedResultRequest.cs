f57502d2c90c578f4c9fa06375d98d423dc128d6
Mr.Lin <linsongbin@126.com>
2024-02-27 16:52:46 +0800

    PredicatePagedResultRequest

---
+++ src/LinFx/Application/Dtos/PredicatePagedResultRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace LinFx.Application.Dtos;
+
+/// <summary>
+/// Simply implements <see cref="IPagedResultRequest"/>.
+/// </summary>
+public class PredicatePagedResultRequest<T> : PagedResultRequest
+{
+    /// <summary>
+    /// 条件表达式
+    /// </summary>
+    public Expression<Func<T, bool>>? Expression { get; set; }
+}
