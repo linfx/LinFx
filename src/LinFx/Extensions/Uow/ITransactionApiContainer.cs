bc94ba2ae3723a8f6bb47717aae2b44bcda408c7
sky <410728115@qq.com>
2024-04-11 21:31:54 +0800

    now

--- src/LinFx/Extensions/Uow/ITransactionApiContainer.cs
+++ src/LinFx/Extensions/Uow/ITransactionApiContainer.cs
@@ -1,12 +1,10 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace LinFx.Extensions.Uow;
+﻿namespace LinFx.Extensions.Uow;
 
 public interface ITransactionApiContainer
 {
-    ITransactionApi FindTransactionApi([NotNull] string key);
+    ITransactionApi FindTransactionApi(string key);
 
-    void AddTransactionApi([NotNull] string key, [NotNull] ITransactionApi api);
+    void AddTransactionApi(string key, ITransactionApi api);
 
-    ITransactionApi GetOrAddTransactionApi([NotNull] string key, [NotNull] Func<ITransactionApi> factory);
+    ITransactionApi GetOrAddTransactionApi(string key, Func<ITransactionApi> factory);
 }
