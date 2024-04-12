8316b19eb174c46931f1f2b97a0bf5ba46d02942
Mr.Lin <linsongbin@126.com>
2024-04-12 08:54:29 +0800

    v8.0.13

--- nupkg/common.ps1
+++ nupkg/common.ps1
@@ -13,13 +13,13 @@ $projects = (
     # linfx
     "src/LinFx",
     "src/LinFx.Extensions.Autofac",
-    "src/LinFx.Extensions.AspNetCore"
-    # "src/LinFx.Extensions.EntityFrameworkCore"
+    "src/LinFx.Extensions.AspNetCore",
+    "src/LinFx.Extensions.EntityFrameworkCore",
     # "src/LinFx.Extensions.EntityFrameworkCore.PostgreSql",
     # "src/LinFx.Extensions.EntityFrameworkCore.Sqlite",
 
     # modules
-    # "src/modules/audit-logging/LinFx.Extensions.AuditLogging",
-    # "src/modules/permission-management/LinFx.Extensions.PermissionManagement",
-    # "src/modules/tenant-management/LinFx.Extensions.TenantManagement"
+    "src/LinFx.Extensions.AuditLogging",
+    "src/LinFx.Extensions.PermissionManagement",
+    "src/LinFx.Extensions.TenantManagement"
 )
